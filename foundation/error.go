4319f532b8788636db00ea33a104b0306cd82566
glj <7909213@qq.com>
2021-01-11 20:29:36 +0800

    add some test code

--- foundation/error.go
+++ foundation/error.go
@@ -58,13 +58,13 @@ func run() error {
 type ErrNegativeSqrt float64
 
 func (e ErrNegativeSqrt) Error() string {
-	return fmt.Sprintf("cannot Sqrt negative number: %v", e)
+	return fmt.Sprintf("cannot Sqrt negative number: %v", float64(e))
 }
 
 func Sqrt(x float64) (float64, error) {
-	//if x < 0 {
-	//	return 0, &ErrNegativeSqrt{}
-	//}
+	if x < 0 {
+		return 0, ErrNegativeSqrt(x)
+	}
 	return math.Sqrt(x), nil
 }
 
