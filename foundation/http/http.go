c3dc5d80e7daf186ac324f66e612a55155f35553
unknown <7909213@qq.com>
2021-09-05 22:48:28 +0800

    add some demo

--- foundation/http/http.go
+++ foundation/http/http.go
@@ -2,10 +2,29 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"io/ioutil"
 	"net/http"
 	"os"
+	"time"
 )
+
+func fetch(url string, ch chan<- string) {
+	start := time.Now()
+	resp, err := http.Get(url)
+	if err != nil {
+		ch <- fmt.Sprint(err) // send to channel ch
+		return
+	}
+	nbytes, err := io.Copy(ioutil.Discard, resp.Body)
+	resp.Body.Close() // don't leak resources
+	if err != nil {
+		ch <- fmt.Sprintf("while reading %s: %v", url, err)
+		return
+	}
+	secs := time.Since(start).Seconds()
+	ch <- fmt.Sprintf("%.2fs %7d %s", secs, nbytes, url)
+}
 
 func main() {
 	fmt.Println(os.Args)
@@ -21,19 +40,13 @@ func main() {
 	}
 	fmt.Println(args)
 
+	start := time.Now()
+	ch := make(chan string)
 	for _, url := range args {
-		resp, err := http.Get(url)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
-			os.Exit(1)
-		}
-		b, err := ioutil.ReadAll(resp.Body)
-		resp.Body.Close()
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
-			os.Exit(1)
-		}
-		fmt.Printf("%s", b)
-
+		go fetch(url, ch) // start a goroutine
 	}
+	for range args {
+		fmt.Println(<-ch) // receive from channel ch
+	}
+	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
 }
---
+++ go.mod
@@ -0,0 +1,16 @@
+module GoStudy
+
+go 1.17
+
+require (
+	github.com/Bowery/prompt v0.0.0-20190916142128-fa8279994f75 // indirect
+	github.com/dchest/safefile v0.0.0-20151022103144-855e8d98f185 // indirect
+	github.com/google/shlex v0.0.0-20191202100458-e7afc7fbc510 // indirect
+	github.com/kardianos/govendor v1.0.9 // indirect
+	github.com/mitchellh/go-ps v1.0.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/yuin/gopher-lua v0.0.0-20210529063254-f4c35e4016d9 // indirect
+	golang.org/x/sys v0.0.0-20210510120138-977fb7262007 // indirect
+	golang.org/x/tools v0.1.5 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
