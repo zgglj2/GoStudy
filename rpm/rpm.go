6f61b5f5b95bf10105e18a4c919929cf3d375e87
glj <7909213@qq.com>
2021-09-23 15:30:53 +0800

    add go-rpmutils demo code

--- rpm/rpm.go
+++ rpm/rpm.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"crypto/md5"
 	"fmt"
+	"io"
 	"os"
+	"strings"
 
 	"github.com/cavaliercoder/go-rpm"
+	"github.com/sassoftware/go-rpmutils"
 )
 
 func ExampleMD5Check(rpm_file string) {
@@ -54,4 +58,60 @@ func main() {
 	rpm_file := "net-tools-1.60-78.el5.x86_64.rpm"
 	ExampleMD5Check(rpm_file)
 	ExamplePackageFile_Files(rpm_file)
+
+	f, err := os.Open(rpm_file)
+	if err != nil {
+		panic(err)
+	}
+	rpm, err := rpmutils.ReadRpm(f)
+	if err != nil {
+		panic(err)
+	}
+	// // Getting metadata
+	// nevra, err := rpm.Header.GetNEVRA()
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// fmt.Println(nevra)
+	// provides, err := rpm.Header.GetStrings(rpmutils.PROVIDENAME)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// fmt.Println("Provides:")
+	// for _, p := range provides {
+	// 	fmt.Println(p)
+	// }
+	// Extracting payload
+	// if err := rpm.ExpandPayload("destdir"); err != nil {
+	// 	panic(err)
+	// }
+	reader, err := rpm.PayloadReader()
+	if err != nil {
+		panic(err)
+	}
+
+	for {
+		header, err := reader.Next()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println(header.Filename(), header.Filesize64())
+		if header.Mode()&0111 != 0 && header.Filesize64() != 0 {
+			if strings.HasPrefix(header.Filename(), "/usr/share/doc") {
+				continue
+			}
+			buf := make([]byte, header.Filesize64())
+			_, err := reader.Read(buf)
+			if err != nil {
+				panic(err)
+			}
+			has := md5.Sum(buf)
+			md5str := fmt.Sprintf("%x", has)
+			fmt.Println(header.Filename(), md5str)
+		}
+	}
 }
