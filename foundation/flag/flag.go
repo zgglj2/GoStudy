76ad0f65307f7fdc90e0cc9860db6a8a06127ae6
glj <7909213@qq.com>
2021-09-24 18:01:06 +0800

    add some flag demo

--- foundation/flag/flag.go
+++ foundation/flag/flag.go
@@ -3,27 +3,64 @@ package main
 import (
 	"flag"
 	"fmt"
+	"time"
 )
 
+type Celsius float64
+type Fahrenheit float64
+
+const (
+	AbsoluteZeroC Celsius = -273.15
+	FreezingC     Celsius = 0
+	BoilingC      Celsius = 100
+)
+
+func CToF(c Celsius) Fahrenheit { return Fahrenheit(c*9/5 + 32) }
+
+func FToC(f Fahrenheit) Celsius { return Celsius((f - 32) * 5 / 9) }
+
+func (c Celsius) String() string { return fmt.Sprintf("%g°C", c) }
+
+type celsiusFlag struct{ Celsius }
+
+func (f *celsiusFlag) Set(s string) error {
+	var unit string
+	var value float64
+	fmt.Sscanf(s, "%f%s", &value, &unit) // no error check needed
+	switch unit {
+	case "C", "°C":
+		f.Celsius = Celsius(value)
+		return nil
+	case "F", "°F":
+		f.Celsius = FToC(Fahrenheit(value))
+		return nil
+	}
+	return fmt.Errorf("invalid temperature %q", s)
+}
+
+func CelsiusFlag(name string, value Celsius, usage string) *Celsius {
+	f := celsiusFlag{value}
+	flag.CommandLine.Var(&f, name, usage)
+	return &f.Celsius
+}
 func main() {
-	// 用户
 	var username string
-	// 密码
 	var password string
-	// 主机名
 	var host string
-	// 端口号
 	var port int
 
-	// StringVar用指定的名称、控制台参数项目、默认值、使用信息注册一个string类型flag，并将flag的值保存到p指向的变量
 	flag.StringVar(&username, "u", "", "用户名,默认为空")
 	flag.StringVar(&password, "p", "", "密码,默认为空")
 	flag.StringVar(&host, "h", "127.0.0.1", "主机名,默认 127.0.0.1")
 	flag.IntVar(&port, "P", 3306, "端口号,默认为空")
+	// eg: 1s  100ms  1.5h  1d
+	period := flag.Duration("period", 1*time.Second, "sleep period")
+	// eg: 100C  100F
+	temp := CelsiusFlag("temp", 20.0, "the temperature")
 
-	// 从arguments中解析注册的flag。必须在所有flag都注册好而未访问其值时执行。未注册却使用flag -help时，会返回ErrHelp。
 	flag.Parse()
+	fmt.Printf("Sleeping for %v...\n", *period)
+	time.Sleep(*period)
 
-	// 打印
-	fmt.Printf("username=%v password=%v host=%v port=%v", username, password, host, port)
+	fmt.Printf("username=%v password=%v host=%v port=%v temp=%v\n", username, password, host, port, temp)
 }
