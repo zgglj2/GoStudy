61d2768dfcd92c5663e0991633a026e550bc740c
zgglj2 <7909213@qq.com>
2021-09-24 20:40:34 +0800

    modify dep demo dir

--- cgo/test_cgo.go
+++ cgo/test_cgo.go
@@ -2,27 +2,15 @@ package main
 
 // #include <stdio.h>
 // #include <stdlib.h>
+//
+// static void myprint(char* s) {
+//   printf("%s\n", s);
+// }
 import "C"
 import "unsafe"
 
-import "fmt"
-
-func Random() int {
-	return int(C.random())
-}
-
-func Seed(i int) {
-	C.srandom(C.uint(i))
-}
-
-func Print(s string) {
-	cs := C.CString(s)
-	defer C.free(unsafe.Pointer(cs))
-	C.fputs(cs, (*C.FILE)(C.stdout))
-}
-
 func main() {
-	fmt.Println(Random())
-
-	Print("hello")
+	cs := C.CString("Hello from stdio")
+	C.myprint(cs)
+	C.free(unsafe.Pointer(cs))
 }
