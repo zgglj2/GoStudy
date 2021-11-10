71883223bd43832675d27cf8a94033b195f56fad
zgglj2 <7909213@qq.com>
2021-11-10 22:21:37 +0800

    add tail demo

---
+++ tail/tail.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/hpcloud/tail"
+)
+
+func main() {
+	config := tail.Config{
+		ReOpen:    true,
+		Follow:    true,
+		Location:  &tail.SeekInfo{Offset: 0, Whence: 2},
+		MustExist: false,
+		Poll:      true,
+	}
+	tails, err := tail.TailFile("my.log", config)
+	if err != nil {
+		fmt.Println("tail file fail, err: ", tails)
+		return
+	}
+	var line *tail.Line
+	var ok bool
+	for {
+		line, ok = <-tails.Lines
+		if !ok {
+			fmt.Println("tail file close reopen, filename: ", tails.Filename)
+			time.Sleep(time.Second)
+			continue
+		}
+		fmt.Println("line: ", line.Text)
+	}
+}
